11346fa0fefb2b57bfab4a4c49a375f4dfc80709
Haoroux <52127278+Haoroux@users.noreply.github.com>
2024-12-14 20:40:55 +0100

    v1.1
    creating more options to be more configurable

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
+#Instructions
+#1)If you’re converting your webcam video then put video_convert to True
+#   Then you have to put an image path into the path variable
+#2)you have to select the size you want for your ascii art with the variables wanted_height and wanted_width
+#3)optional but you can select wich grayscale you’re using for your art by changing the variable used_gscale by gscale2
+
+#the video convertor will be added in near future 
+
 # importing libs
 from cv2 import VideoCapture
 from pynput.keyboard import *
-
 import cv2 as cv
 import numpy as np
-import time
 
 
-# var
-i = 0 
+# config var
+video_convert = False
+
 path = 'C:/Users/rafae/Documents/prog/python/visual/totoro.webp'
-path2 = 'C:/Users/rafae/Documents/prog/python/visual/amous.jpg'
 
-image = cv.imread(path2)
-gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)  # rend l’image grise
+wanted_height = 70
+wanted_width = 140
+
 
 cam = VideoCapture(0, cv.CAP_DSHOW)
+# var
 
 
-# mesures
-wanted_height = 70
-wanted_width = 140  
 
 
 # arrays
@@ -29,6 +34,7 @@ gscale2 = " .',;:clodxkO0KXNWM"
 
 used_gscale = gscale
 divider = 255 / len(used_gscale)
+
 
 
 def img_ascii_convertor(image_to_convert):
@@ -72,7 +78,15 @@ def display_ascii(ascii_array, rows, cols):
     return ascii_str
 
 
-while True:
+if video_convert == False:
+    image = cv.imread(path)
+    gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)  # rend l’image grise
+    print(img_ascii_convertor(image))
+    with open("ascii_art.txt", "w") as file:
+        file.write(img_ascii_convertor(image))
+
+
+while video_convert == True:
     result, v_image = cam.read()
     
     if result:
